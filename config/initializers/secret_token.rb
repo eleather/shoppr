0cd8fdd6c388a5246632bf3f032816138c8c0fe4
Emily L <peppermintdove@gmail.com>
2011-04-15 15:28:00 -0700

    empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoppr::Application.config.secret_token = 'be1d38e68cb78ae82abfb158f869c211c77f7926608c999b3233fae0a34d7e60c2fdc622a93c2697ece253d7f380dfbcbcd074edfdf1993b4384e8d4c0b2118a'
