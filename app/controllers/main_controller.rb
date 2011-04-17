22698c2cfb792275b2884e053f8f70423a077ad5
Emily L <peppermintdove@gmail.com>
2011-04-17 12:10:50 -0700

    hacky version - using some twitter code as model

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,9 +2,9 @@ class MainController < ApplicationController
   
   def index
     @lists = [
-      {:name => "List A", :category => {:name => "Category A"}},
-      {:name => "List B", :category => {:name => "Category B"}},
-      {:name => "List C", :category => {:name => "Category B"}}
+      {:name => 'List A', :category => {:name => 'Category A'}, :due_at => 'today', :description => 'Desc A'},
+      {:name => 'List B', :category => {:name => 'Category B'}, :due_at => 'tomorrow', :description => 'Desc B'},
+      {:name => 'List C', :category => {:name => 'Category B'}, :due_at => 'friday', :description => 'Desc C'}
     ]
   end
   
