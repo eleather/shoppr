e1ed95e62262ff8b4d0569ae19fd79b02099fffc
Emily L <peppermintdove@gmail.com>
2011-04-17 09:01:23 -0700

    add fake content to main/index

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,2 +1,11 @@
 class MainController < ApplicationController
+  
+  def index
+    @lists = [
+      {:name => "List A", :category => {:name => "Category A"}},
+      {:name => "List B", :category => {:name => "Category B"}},
+      {:name => "List C", :category => {:name => "Category B"}}
+    ]
+  end
+  
 end
